3b5d408c8d41f2cb01812e25f822dfaa080103d1
feuzeu <thierry.feuzeu@gmail.com>
2016-10-15 20:39:52 -0400

    Update following an update to the config in the jaxon-core package.

--- src/Jaxon.php
+++ src/Jaxon.php
@@ -27,5 +27,47 @@ class Jaxon
      */
     public function setup()
     {
+        // This function should be called only once
+        if(($this->setupCalled))
+        {
+            return;
+        }
+        $this->setupCalled = true;
+
+        // Use the Composer autoloader
+        $this->jaxon->useComposerAutoloader();
+        // Jaxon library default options
+        $this->jaxon->setOptions(array(
+            'js.app.extern' => !config('app.debug', false),
+            'js.app.minify' => !config('app.debug', false),
+            'js.app.uri' => asset('jaxon/js'),
+            'js.app.dir' => public_path('jaxon/js'),
+        ));
+
+        // Jaxon library user options
+        $libConfig = config('jaxon.lib', array());
+        $this->jaxon->setOptions($libConfig);
+
+        // Jaxon application config
+        $controllerDir = config('jaxon.app.dir', app_path('Jaxon/Controllers'));
+        $namespace = config('jaxon.app.namespace', '\\Jaxon\\App');
+        $excluded = config('jaxon.app.excluded', array());
+        // The public methods of the Controller base class must not be exported to javascript
+        $controllerClass = new \ReflectionClass('\\Jaxon\\Laravel\\Controller');
+        foreach ($controllerClass->getMethods(\ReflectionMethod::IS_PUBLIC) as $xMethod)
+        {
+            $excluded[] = $xMethod->getShortName();
+        }
+
+        // The request URI can be set with a Laravel route
+        if(!$this->jaxon->hasOption('core.request.uri'))
+        {
+            if(($route = config('jaxon.app.route', null)))
+                $this->jaxon->setOption('core.request.uri', route($route));
+            else
+                $this->jaxon->setOption('core.request.uri', 'jaxon');
+        }
+        // Register the default Jaxon class directory
+        $this->jaxon->addClassDir($controllerDir, $namespace, $excluded);
     }
 }
--- src/JaxonServiceProvider.php
+++ src/JaxonServiceProvider.php
@@ -44,40 +44,9 @@ class JaxonServiceProvider extends ServiceProvider
         // Register the Jaxon singleton
         $this->app->singleton('jaxon', function ($app)
         {
-            // Jaxon application config
-            $requestRoute = config('jaxon.app.route', 'jaxon');
-            $controllerDir = config('jaxon.app.dir', app_path('Jaxon/Controllers'));
-            $namespace = config('jaxon.app.namespace', '\\Jaxon\\App');
-
-            $excluded = config('jaxon.app.excluded', array());
-            // The public methods of the Controller base class must not be exported to javascript
-            $controllerClass = new \ReflectionClass('\\Jaxon\\Laravel\\Controller');
-            foreach ($controllerClass->getMethods(\ReflectionMethod::IS_PUBLIC) as $xMethod)
-            {
-                $excluded[] = $xMethod->getShortName();
-            }
-
-            $jaxon = jaxon();
-            // Use the Composer autoloader
-            $jaxon->useComposerAutoloader();
-            // Jaxon library default options
-            $jaxon->setOptions(array(
-                'js.app.extern' => !config('app.debug', false),
-                'js.app.minify' => !config('app.debug', false),
-                'js.app.uri' => asset('jaxon/js'),
-                'js.app.dir' => public_path('jaxon/js'),
-            ));
-            // Jaxon library user options
-            $jaxon->readConfigFile(base_path('config/jaxon.php'), 'lib');
-            // The request URI can be set with a Laravel route
-            if(!$jaxon->hasOption('core.request.uri'))
-            {
-                $jaxon->setOption('core.request.uri', route($requestRoute));
-            }
-            // Register the default Jaxon class directory
-            $jaxon->addClassDir($controllerDir, $namespace, $excluded);
-
-            return new Jaxon();
+            $jaxon = new Jaxon();
+            $jaxon->setup();
+            return $jaxon;
         });
     }
 
