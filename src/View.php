4db46b1bddc8fec69a89667ecf66a60eac20e283
feuzeu <thierry.feuzeu@gmail.com>
2017-03-13 00:15:51 -0400

    Now using core Facade and Store classes to render views.

--- src/View.php
+++ src/View.php
@@ -2,33 +2,21 @@
 
 namespace Jaxon\Laravel;
 
-use Illuminate\Pagination\LengthAwarePaginator as Paginator;
+use Jaxon\Module\View\Store;
+use Jaxon\Module\View\Facade;
 
-class View
+class View extends Facade
 {
     /**
-     * Make a piece of data available for all views
-     *
-     * @param string        $name            The data name
-     * @param string        $value            The data value
+     * Render a view
      * 
-     * @return void
-     */
-    public function share($name, $value)
-    {
-        view()->share($name, $value);
-    }
-
-    /**
-     * Render a template
-     *
-     * @param string        $template        The template path
-     * @param string        $data            The template data
+     * @param Store         $store        A store populated with the view data
      * 
-     * @return mixed        The rendered template
+     * @return string        The string representation of the view
      */
-    public function render($template, array $data = array())
+    public function make(Store $store)
     {
-        return view($template, $data);
+        // Render the template
+        return trim(view($store->getViewPath(), $store->getViewData()), " \t\n");
     }
 }
